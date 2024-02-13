9e8c1991254b5caee3a088f9c7f4d88f0e095eea
hhhhf8 <159798309+hhhhf8@users.noreply.github.com>
2024-02-13 08:36:10 +0800

    Add files via upload

--- getindizes.m
+++ getindizes.m
@@ -1,18 +1,19 @@
-function [i_2d,L_2d,II_2d,i_3d,j_3d,L_3d,II_3d,J_3d] = getindizes()
+function [i_tilde_2d,l_2d,lli_2d,i_tilde_3d,j_tilde_3d,l_3d,lli_3d,llj_3d] = getindizes()
 global const
 %% Aufgabe 2 von Sifeng Li
+%% lli = [2l+i] , llj = [2l+j]
 
 n = const.n;
 
 l0 = 0:n-1;
 i0 = 0:3;
 
-    [i_2d,L_2d] = ndgrid(i0,l0);
-     II_2d =2*L_2d+i_2d;
+    [i_tilde_2d,l_2d] = ndgrid(i0,l0);
+     lli_2d =2*l_2d+i_tilde_2d;
     
 
-    [i_3d,j_3d,L_3d] = ndgrid(i0,i0,l0);
-    II_3d = 2*L_3d+i_3d;
-    J_3d = 2*L_3d+j_3d;
+    [i_tilde_3d,j_tilde_3d,l_3d] = ndgrid(i0,i0,l0);
+    lli_3d = 2*l_3d+i_tilde_3d;
+    llj_3d = 2*l_3d+j_tilde_3d;
     
 end
