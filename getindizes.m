76663ec5bde6fb17dc21c8acfdd6d477bcffd9fe
QianboFang <150347781+QianboFang@users.noreply.github.com>
2024-02-14 14:20:49 +0100

    Update getindizes.m

--- getindizes.m
+++ getindizes.m
@@ -5,15 +5,15 @@ global const
 
 n = const.n;
 
-l0 = 1:n;
-i0 = 1:4;
+l0 = 0:n-1;
+i0 = 0:3;
 
-    [i_tilde_2d,l_2d] = ndgrid(i0,l0);
-     lli_2d =2*l_2d+i_tilde_2d;
-    
+[i_tilde_2d,l_2d] = ndgrid(i0,l0);
+lli_2d =2*l_2d+i_tilde_2d;
 
-    [i_tilde_3d,j_tilde_3d,l_3d] = ndgrid(i0,i0,l0);
-    lli_3d = 2*l_3d+i_tilde_3d;
-    llj_3d = 2*l_3d+j_tilde_3d;
-    
+
+[i_tilde_3d,j_tilde_3d,l_3d] = ndgrid(i0,i0,l0);
+lli_3d = 2*l_3d+i_tilde_3d;
+llj_3d = 2*l_3d+j_tilde_3d;
+
 end
