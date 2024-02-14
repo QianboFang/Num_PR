84096ad6fe37206fc6a4aa7e2595bf682bbf124a
QianboFang <150347781+QianboFang@users.noreply.github.com>
2024-02-14 14:27:28 +0100

    Update getvq.m

--- getvq.m
+++ getvq.m
@@ -3,14 +3,22 @@ global const
 %-------------------------------------------------------------------------
                            % Aufgabe 5 c %
 %-------------------------------------------------------------------------
+
+% Holen der Konstanten
 n = const.n;
+N = 2*n+2;
+
+% Holen von qbar
 qbar = getqbar();
 
-vq = zeros(2*n + 2, 1); 
-for i = 1:n
-    vq(2*i-1:2*i+2) = vq(2*i-1:2*i+2) + qbar(:,:,1);
-end
+% Holen der Indizes
+[i_tilde_2d, l_2d, lli_2d, i_tilde_3d, j_tilde_3d, l_3d, lli_3d, llj_3d] = getindizes();
 
-vq = sparse(vq);
+
+qbar_vec = reshape(qbar, [], 1);
+
+% Erstellung der dünnbesetzten Matrix mit Hilfe der sparse-Funktion
+vq = sparse(lli_2d+1, 1, qbar_vec, 8, 1);
+vq = full(vq);
 
 end
