48b246d508adc6d262f7b4894d7cbad033e51a13
hhhhf8 <159798309+hhhhf8@users.noreply.github.com>
2024-02-26 03:25:53 +0800

    Add files via upload

---
+++ Parameters Aufgabe15.m
@@ -0,0 +1,5 @@
+%% Aufgabe 15
+mu = @(x) x.^0; % (Längenspezifische Masse in kg/m)
+Efun = @(x) x.^0;% (Elastizitätsmodul in N/m2),
+Ifun = @(x)  x.^0;%(Flächenträgheitsmoment in m4),
+qfun = @(x) x.^0;% (Streckenlast in N/m)
