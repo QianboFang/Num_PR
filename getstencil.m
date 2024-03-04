61f4c1bcbc81bbcd76fafae4d643cbae5c68a183
mikado16 <159798770+mikado16@users.noreply.github.com>
2024-03-04 11:17:43 +0100

    getstencil.m

--- getstencil.m
+++ getstencil.m
@@ -1,4 +1,4 @@
-function [V,V_hat,s] = getstencil(n)
+function [s, s_3d, s_4d] = getstencil(n)
 %%  Aufgabe 16
 
 i_hat = 0:n; i_hat = i_hat(:);
@@ -8,3 +8,9 @@ x_hat = 0:1/n:1; x_hat = x_hat(:);
 V = x_hat.^repmat(0:n,n+1,1);
 s = V'\(V_hat);
 s = s';
+
+% Aufgabe 18
+s_3d = reshape(s,1,1,n_tilde+1);
+s_4d = reshape(s,1,1,1,n_tilde+1);
+
+end
