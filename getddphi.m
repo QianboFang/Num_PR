f725245810c7611660f190499e491b429a42ca3a
mikado16 <159798770+mikado16@users.noreply.github.com>
2024-03-04 11:12:35 +0100

    getddphi.m

--- getddphi.m
+++ getddphi.m
@@ -4,6 +4,7 @@ global const
 % AUfgabe 17 
 
 n_tilde = const.n_tilde;
+n = const.n;
 
 ddphi_1 = @(x) -6 + 12.*x;
 ddphi_2 = @(x) -4 +6.*x;
@@ -14,4 +15,11 @@ ddphi = [transpose(ddphi_1(x_k(:)));
     transpose(ddphi_2(x_k(:)));
     transpose(ddphi_3(x_k(:)));
     transpose(ddphi_4(x_k(:)))];
+
+% Aufgabe 18
+[~,~,~,i,j,~,~,~,i_v,~,~] = getindizes();
+ddphi4di = reshape(ddphi(i,:),4,4,n,n_tilde+1);
+ddphi4dj = reshape(ddphi(j,:),4,4,n,n_tilde+1);
+ddphi3di_v = reshape(ddphi(i_v,:),4,n,n_tilde+1);
+
 end
