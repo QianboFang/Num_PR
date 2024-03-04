691491c59f76e85f5953f0ccd0eb773a212c3458
mikado16 <159798770+mikado16@users.noreply.github.com>
2024-03-04 11:13:34 +0100

    Update getphi.m

--- getphi.m
+++ getphi.m
@@ -3,6 +3,7 @@ global const
 
 % Aufgabe 17 
 n_tilde = const.n_tilde;
+n = conct.n;
 
 phi_1 = @(x) 1 - 3.*x.^2 + 2.*x.^3;
 phi_2 = @(x) x - 2.*x.^2 + x.^3;
@@ -13,5 +14,11 @@ phi = [transpose(phi_1(x_k(:)));
     transpose(phi_2(x_k(:)));
     transpose(phi_3(x_k(:)));
     transpose(phi_4(x_k(:)))];
+
+% Aufgabe 18 
+[~,~,~,i,j,~,~,~,i_v,~,~] = getindizes(); % die Indizes werden aufgerufen
+phi4di = reshape(phi(i,:),4,4,n,n_tilde+1);
+phi4dj = reshape(phi(j,:),4,4,n,n_tilde+1);
+phi3di_v = reshape(phi(i_v,:),4,n,n_tilde+1);
 
 end
