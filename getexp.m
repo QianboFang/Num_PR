063736121fa25c0b55fb68462c1565e12ed81133
mikado16 <159798770+mikado16@users.noreply.github.com>
2024-03-04 11:21:41 +0100

    getexp.m

--- getexp.m
+++ getexp.m
@@ -1,8 +1,9 @@
-function exp = getexp
+function [expi_v, expij] = getexp
 
 % Aufgabe 17
 
-[~,~,~,i,j,~,~,~] = getindizes();
-exp = (i == 2 | i == 4) + (j == 2 | j == 4);
+[~,~,~,i,j,~,~,~,i_v,~,~] = getindizes();
+expi_v = (i_v == 2 | i_v == 4);
+expij = (i == 2| i == 4) + (j == 2 | j == 4);
 
 end
