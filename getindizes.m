a4ac1ef97d7c21d8dc08a7c3c9defff7ce915efa
mikado16 <159798770+mikado16@users.noreply.github.com>
2024-03-04 11:20:39 +0100

    getindizes.m

--- getindizes.m
+++ getindizes.m
@@ -1,4 +1,4 @@
-function [i_tilde_2d,l_2d,lli_2d,i_tilde_3d,j_tilde_3d,l_3d,lli_3d,llj_3d] = getindizes()
+function [i_tilde_2d,l_2d,lli_2d,i_tilde_3d,j_tilde_3d,l_3d,lli_3d,llj_3d,I_v,L_v,ll_i_v] = getindizes()
 global const
 %% Aufgabe 2 von Sifeng Li
 %% lli = [2l+i] , llj = [2l+j]
@@ -15,5 +15,8 @@ lli_2d =2*l_2d+i_tilde_2d;
 [i_tilde_3d,j_tilde_3d,l_3d] = ndgrid(i0,i0,l0);
 lli_3d = 2*l_3d+i_tilde_3d;
 llj_3d = 2*l_3d+j_tilde_3d;
+
+[I_v,~,L_v] = ndgrid(i0,0,l0);
+ll_i_v = 2*L_v + I_v;
 
 end
