6225e2967a732d154d204b6492472bd88c2fe785
mikado16 <159798770+mikado16@users.noreply.github.com>
2024-03-04 11:16:28 +0100

    getTinv.m

--- getTinv.m
+++ getTinv.m
@@ -1,4 +1,4 @@
-function Tinv = getTinv()
+function [Tinv_3d, Tinv_4d, Tinv] = getTinv()
 global const
 
 % Aufgabe 17
@@ -9,6 +9,12 @@ n_tilde = const.n_tilde;
 
 x_k = linspace(0,1,n_tilde+1);
 x = linspace(0,l,n+1);
-x_l = x(1:end-1);
-Tinv = geth().*x_k + transpose(x_l) ;
+x_l = transpose(x(1:end-1));
+[~,~,h] = geth();
+Tinv = h.*x_k + x_l;
+
+% Aufgabe 18
+Tinv_3d = reshape(Tinv,1,n,n_tilde+1);
+Tinv_4d = reshape(Tinv,1,1,n,n_tilde+1);
+
 end
