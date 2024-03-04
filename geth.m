502d35dd5f63fdbca02a46a4c9e9c2abdb56e780
mikado16 <159798770+mikado16@users.noreply.github.com>
2024-03-04 11:14:51 +0100

    geth.m

--- geth.m
+++ geth.m
@@ -1,4 +1,4 @@
-function h = geth()
+function [h_l2d,h_l3d,h] = geth()
 global const
 
 % Aufgabe 17 
@@ -7,6 +7,9 @@ n = const.n;
 l = const.l;
 
 x = linspace(0,l,n+1);
-h = transpose(x(2:end) - x(1:end-1));
+h = (x(2:end) - x(1:end-1))';
+% Aufgabe 18
+h_l2d = reshape(h,1,n);
+h_l3d = reshape(h,1,1,n);
 
 end
