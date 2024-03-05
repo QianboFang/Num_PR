e6f032edac557d70cf3bab5bc9a44226a7ede0aa
mikado16 <159798770+mikado16@users.noreply.github.com>
2024-03-05 17:23:42 +0100

    getstencil.m

--- getstencil.m
+++ getstencil.m
@@ -1,11 +1,11 @@
-function [s, s_3d, s_4d] = getstencil(n)
+function [s, s_3d, s_4d] = getstencil(n_tilde)
 %%  Aufgabe 16
 
-i_hat = 0:n; i_hat = i_hat(:);
+i_hat = 0:n_tilde; i_hat = i_hat(:);
 V_hat = 1./(1+i_hat);
 
-x_hat = 0:1/n:1; x_hat = x_hat(:);
-V = x_hat.^repmat(0:n,n+1,1);
+x_hat = 0:1/n_tilde:1; x_hat = x_hat(:);
+V = x_hat.^repmat(0:n_tilde,n_tilde+1,1);
 s = V'\(V_hat);
 s = s';
 
